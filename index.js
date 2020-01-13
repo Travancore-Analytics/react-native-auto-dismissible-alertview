f7ee7926a981c73e0b7c4c9aaea7bddf6e929f2d
Anish MK <anish.mk@teamta.in>
2020-01-13 20:39:32 +0530

    Implemented  customised alert

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-import { NativeModules } from "react-native";
+import { NativeModules, Platform} from "react-native";
 
 const { AlertView } = NativeModules;
 
 export default {
   showAlert(title,message,cancelable,buttons,callback) {
     return AlertView.showAlert(title,message,cancelable,buttons,callback);
+  },
+  showCustomizedAlert(title,message,buttonText,style,callback){
+    if(Platform.OS == "android"){
+      return AlertView.showCustomizedAlert(title,message,buttonText,style,callback);
+    }
   }
 };
